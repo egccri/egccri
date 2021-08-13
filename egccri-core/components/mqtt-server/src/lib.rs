5eedb9b11640b2ee045f782423a14b6890e57406
zifeng <zifeng.1024@gmail.com>
2021-08-13 09:21:39 +0800

    mqtt server.

--- egccri-core/components/mqtt-server/src/lib.rs
+++ egccri-core/components/mqtt-server/src/lib.rs
@@ -1,7 +1,11 @@
 mod mqtt;
 
 pub mod server {
-    pub fn say_hi() {
-        println!("I'm mqtt server.")
+    use tokio::sync::{broadcast, mpsc};
+
+    pub fn run() {
+        println!("I'm mqtt server.");
+        let (ctrl_c_tx, ctrl_c_rx) = broadcast::channel(5);
+        let (client_tx, client_rx) = mpsc::channel(32);
     }
 }
